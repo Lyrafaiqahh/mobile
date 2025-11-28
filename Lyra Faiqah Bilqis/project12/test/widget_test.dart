6887d409c45c209d8f446e93c15c882ac1612858
Lyra Faiqah <lyrafaiqah@gmail.com>
2025-11-28 12:36:36 +0700

    praktikum 8 JSON

--- Lyra Faiqah Bilqis/project12/test/widget_test.dart
+++ Lyra Faiqah Bilqis/project12/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:project12/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(NotesLyraApp());
+    await tester.pumpWidget(WireMockApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
